e47b9956e48693b80c61b9441d15863388638471
shlyapusataya <dsevriukov@edu.cdv.pl>
2024-10-26 12:05:23 +0200

    Getting leaderboard system fix

--- Assets/Scripts/ScoreBase.cs
+++ Assets/Scripts/ScoreBase.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
+using TMPro;
+using System.Text.RegularExpressions;
 
 public class ScoreBase : MonoBehaviour
 {
     private string url = "http://localhost:3000/";
+    [SerializeField] private TextMeshProUGUI scoreboard;
     private void Start()
     {
         StartCoroutine(GetInfoFromUrl());
@@ -24,17 +28,29 @@ public class ScoreBase : MonoBehaviour
         else
         {
             string scoredata = request.downloadHandler.text;
-            string[] highscores = scoredata.Split("; ");
+            string[] highscores = scoredata.Split(new[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+            string finalDisplayText = "";
 
-            foreach(string highscore in highscores)
+            foreach (string highscore in highscores)
             {
-                string[] fields = highscore.Split(", ");
 
-                string name = fields[0];
-                int score = int.Parse(fields[1]);
+                string[] fields = Regex.Split(highscore, @"\s*,\s*");
 
-                Debug.Log(name + ": " + score);
+                if (fields.Length == 2)
+                {
+                    string name = fields[0].Trim();
+                    string score = fields[1].Trim();
+
+                    finalDisplayText += $"{name} - {score}\n";
+                }
+                else
+                {
+                    Debug.LogWarning("Invalid data format for highscore entry: " + highscore);
+                    Debug.LogWarning("Fields length: " + fields.Length + ", Content: " + string.Join(", ", fields));
+                }
             }
+
+            scoreboard.text = finalDisplayText;
         }
     }
 }
